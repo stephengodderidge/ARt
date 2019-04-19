398f9d634d566b6fbe7a861cf189b3f93ad44318
Stephen Godderidge <stephen.godderidge@parivedasolutions.com>
2019-04-19 15:37:15 -0400

    Initial Commit

---
+++ drawingman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drawingman
+//
+//  Created by Stephen Godderidge on 4/19/19.
+//  Copyright © 2019 Stephen Godderidge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
