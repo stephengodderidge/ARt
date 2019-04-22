185d42f18239192cd0c9cbc4fbc4badfb85a8e0b
Stephen Godderidge <stephen.godderidge@parivedasolutions.com>
2019-04-22 10:52:59 -0400

    added base files for AR drawing kit

--- drawingman/ViewController.swift
+++ drawingman/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate{
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var draw: UIButton!
 
+    let configuration = ARWorldTrackingConfiguration()
+    
+    var currentColor = UIColor.red;
+    var currentSize = CGFloat(0.02);
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.showsStatistics = true
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        
     }
 
+    
+    func getCurrentPositionOfCamera()->SCNVector3? {
+        guard let pointOfView = self.sceneView.pointOfView else {return nil}
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let position = orientation + location
+        return position
+    }
+    
+    func renderDrawingTip(position: SCNVector3){
+        let tipNode = SCNNode(geometry: SCNSphere(radius: self.currentSize))
+        tipNode.position = position
+        tipNode.name = "tip"
+        
+        self.sceneView.scene.rootNode.enumerateChildNodes({(node, _) in
+            if node.name == "tip" {
+                node.removeFromParentNode()
+            }
+        })
+        
+        self.sceneView.scene.rootNode.addChildNode(tipNode)
+        tipNode.geometry?.firstMaterial?.diffuse.contents = self.currentColor;
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        if let currentPosition = getCurrentPositionOfCamera(){
+            DispatchQueue.main.async{
+                if self.draw.isHighlighted{
+                    let sphereNode = SCNNode(geometry: SCNSphere(radius: self.currentSize))
+                    sphereNode.position = currentPosition
+                    self.sceneView.scene.rootNode.addChildNode(sphereNode)
+                    sphereNode.geometry?.firstMaterial?.diffuse.contents = self.currentColor;
+                } else {
+                    self.renderDrawingTip(position: currentPosition)
+                }
+            }
+        }
+    }
+    
 
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
